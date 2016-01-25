d0913d18ff4788424751da872ac45c19ffba6eaf
Kevin Tessier <tess0112@algonquinlive.com>
2016-01-25 12:40:59 -0500

    In-class walkthrough stuff.

---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObject : MonoBehaviour {
+    // Value given by editer.
+    public GameObject prefabToSpawn = null;
+    GameObject _spawnedObject = null;
+
+	// Use this for initialization
+	void Start () {
+        _spawnedObject = GameObject.Instantiate(prefabToSpawn) as GameObject;
+        _spawnedObject.name = "No Doge, No";
+        // Setting ourselves to be the parent
+        _spawnedObject.transform.parent = transform;
+        _spawnedObject.transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
